077701dc6e001137620dbe5e2c5b2a176f9440fc
Darya <daryabobur@gmail.com>
2022-10-02 21:40:22 +0300

    Create TrendingMoviesItem.jsx

---
+++ src/components/TrendingMoviesItem/TrendingMoviesItem.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router-dom";
+
+const TrendingMoviesItem = ({ items }) => {
+    const item = items.data.results;
+return (
+    <ul>
+        {item.map(({title, id}) => (
+        <li  key={id}>
+        <NavLink><h2>{title}</h2></NavLink>
+        </li>
+        ))} 
+    </ul>
+)
+}
+
+export default TrendingMoviesItem;
