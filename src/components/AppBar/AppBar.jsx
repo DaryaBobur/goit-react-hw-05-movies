cd16ec3ff3d9f586abb9ff5384ba126a9c1eb540
Darya <daryabobur@gmail.com>
2022-10-02 21:40:10 +0300

    Create AppBar.jsx

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,19 @@
+import { NavLink } from "react-router-dom";
+
+const navItems = [
+    { href: '/', text: 'Home' },
+    { href: 'movies', text: 'Movies', },
+  ];
+
+
+  const AppBar = () => {
+    return (
+        <div>
+            {navItems.map(({ href, text}) => (
+                <NavLink to={href} key={href}>{text}</NavLink>
+            ))}
+        </div>
+     )
+  }
+
+  export default AppBar;
