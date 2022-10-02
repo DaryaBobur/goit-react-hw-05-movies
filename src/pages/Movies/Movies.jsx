19ea7d628a26b4be12ac0a1f862d1f4c7f418073
Darya <daryabobur@gmail.com>
2022-10-02 21:40:29 +0300

    Create Movies.jsx

---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,76 @@
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import AppBar from "components/AppBar/AppBar";
+import FormSearchMovies from "components/FormSearchMovies/FormSearchMovies";
+import { api } from "services/getSearchMovies";
+import TrendingMoviesItem from "components/TrendingMoviesItem/TrendingMoviesItem";
+import ButtonLoadMore from "components/ButtonLoadMore/ButtonLoadMore";
+import Loader from "components/Loader/Loader";
+
+
+
+
+
+const Movies = () => {
+    const [items, setItems] = useState([]);
+    const [query, setQuery] = useState('');
+    const [page, setPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
+    const onChangeQuery = query => {
+      setQuery(query);
+      setPage(1);
+      setItems([])
+    };
+
+    useEffect(() => {
+  
+        const getSearchMovies = async () => {
+          const params = { query: query, page };
+          if(!query) {
+            return;
+          }      
+    
+          try {
+            setIsLoading(true);
+            const data = await api.getSearchQueryMovies(params);
+    
+            // setItems((prevItems) => {
+            //     // console.log({...prevItems, ...prevItems.data.results})
+            // //   return {...prevItems, ...data.results}
+            // })
+            setItems(data)
+            } 
+            catch (error) {
+              setError(error);
+            } 
+            finally {
+              setIsLoading(false);
+          }
+        }
+    
+        getSearchMovies();
+    
+      }, [page, query])
+    
+      const loadMore =() => {
+        setPage((prevPage) => prevPage + 1)
+      }
+    
+
+
+    return (
+        <>
+        <AppBar/>
+        <FormSearchMovies onSubmit={onChangeQuery}/>
+        {items.length !== 0 && <TrendingMoviesItem items={items} />}
+        {items.length !== 0 && <ButtonLoadMore onClick={loadMore} />}
+        {isLoading && <Loader/>}
+        {error && <p>Please try again later!</p>}
+        <Outlet/>
+        </>
+    )
+}
+
+export default Movies;
