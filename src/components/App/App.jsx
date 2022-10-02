0f674fe30cb695bbbbe61a3a3e085a3fe7864c18
Darya <daryabobur@gmail.com>
2022-10-02 21:53:59 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "pages/Home/Home";
 import Movies from "pages/Movies/Movies";
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
