2c8ee71009ed3bf5290a28e3496d4052c04d829f
Darya <daryabobur@gmail.com>
2022-10-05 22:14:32 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,28 @@ import { Outlet } from "react-router-dom";
 import AppBar from "components/AppBar/AppBar";
 import Loader from "components/Loader/Loader";
 import { getSearchTrendingMovies } from "services/getSearchMovies";
-import TrendingMoviesItem from "components/TrendingMoviesItem/TrendingMoviesItem";
-import MovieDetails from "components/MovieDetails/MovieDetails";
+import MoviesListTrending from "components/MoviesListTrending/MoviesListTrending";
+import MovieDetails from "pages/MovieDetails/MovieDetails";
+
 const Home = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
 
+
     useEffect(() => {
+
         const fetchTrendingMovies = async () => {
             setIsLoading(true);
+
+          
             try {
+                setItems(prev => ([...prev]))
                const data = await getSearchTrendingMovies();
+               console.log(data)
                setItems(data);
-               
+            
         } catch (error) {
                 setError(error)
             } finally {
@@ -25,13 +32,15 @@ const Home = () => {
             }
         }
 
-            fetchTrendingMovies()
+            fetchTrendingMovies();
     }, [])
 
+console.log(items.data)
     return (
         <>
             <AppBar/>
-            {items.length !== 0 && <TrendingMoviesItem items={items}/>}
+            {items.length !== 0 && <MoviesListTrending items={items}/>}
+
             {isLoading && <Loader/>}
             {error && <p>Please try again later!</p>}
             <MovieDetails/>
