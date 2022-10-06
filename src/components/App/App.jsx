82d2eeb6f8a85b48f5acd175c0dce8c75b032e0e
Darya <daryabobur@gmail.com>
2022-10-06 22:12:45 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,38 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "pages/Home/Home";
-import Movies from "pages/Movies/Movies";
-import MovieDetails from "pages/MovieDetails/MovieDetails";
-import Layout from "components/Layout/Layout";
-import CastMovie from "components/MoreInfoAboutMovie/CastMovie";
-import ReviewsMovie from "components/MoreInfoAboutMovie/ReviewsMovie";
+import { lazy, Suspense } from "react";
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
+const Home = lazy(() => import("pages/Home/Home"));
+const Movies = lazy(() => import("pages/Movies/Movies"));
+const MovieDetails = lazy(() => import("pages/MovieDetails/MovieDetails"));
+const SharedLayout = lazy(() => import("components/SharedLayout/SharedLayout"));
+const CastMovie = lazy(() => import("components/MoreInfoAboutMovie/CastMovie"));
+const ReviewsMovie = lazy(() => import("components/MoreInfoAboutMovie/ReviewsMovie"));
+
+
 const App = () => {
   return (
     <>
+    <Suspense>
         <Routes>
-            <Route path="/" element={<Layout />}/>
+            <Route path="/" element={<SharedLayout />}/>
             <Route index element={<Home />}/>
-            <Route path=":movieId" element={<MovieDetails />}>
+            <Route path="/:movieId" element={<MovieDetails />}>
               <Route path="cast" element={<CastMovie />}/>
               <Route path="reviews" element={<ReviewsMovie />}/>
             </Route>
-            <Route path="/movies" element={<Movies />}/>
-            <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="/movies" element={<Movies />}/>      
+            <Route path="/movies/:movieId" element={<MovieDetails />}>
               <Route path="cast" element={<CastMovie />}/>
               <Route path="reviews" element={<ReviewsMovie />}/>
             </Route>
+  
             <Route path="*" element={<Home />} />
         </Routes>
-          
+        </Suspense>
         <ToastContainer 
           autoClose={3000} 
           theme={'colored'}
