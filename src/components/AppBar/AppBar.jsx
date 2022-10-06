f5fb9f1abe6e98ee62d2b7aa96ecd04629df9da2
Darya <daryabobur@gmail.com>
2022-10-06 22:12:47 +0300

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,13 +2,13 @@ import { NavLink } from "react-router-dom";
 
 const navItems = [
     { href: '/', text: 'Home' },
-    { href: 'movies', text: 'Movies' },
+    { href: '/movies', text: 'Movies' },
 ];
 
 const AppBar = () => {
     return (
         <div>
-            {navItems.map(({ href, text}) => (
+            {navItems.map(({ href, text }) => (
                 <NavLink to={href} key={href}>{text}</NavLink>
             ))}
         </div>
