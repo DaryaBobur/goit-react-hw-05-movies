c8409f7e5282c604b5757dbf1ff082d5185b5bc7
Darya <daryabobur@gmail.com>
2022-10-06 22:13:06 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,20 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const MoviesList = ({ items }) => {
-
     const item = items.data.results;
 
+    const location = useLocation();
+
 return (
-    <>
-    <h1>Trending movies</h1>
     <ul>
         {item.map(({title, id}) => (
         <li key={id}>
-        <NavLink to={`${id}`}><h2>{title}</h2></NavLink>
+        <NavLink state={{from: location}} to={`${id}`}><h2>{title}</h2></NavLink>
         </li>
         ))} 
     </ul>
-    </>
 )
 }
 
