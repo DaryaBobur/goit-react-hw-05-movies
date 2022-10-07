f836ed39745cf3257f66d9f57affaa41a10af70e
Darya <daryabobur@gmail.com>
2022-10-07 13:12:26 +0300

    Update Movies.jsx

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,70 +1,59 @@
-
 import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import AppBar from "components/AppBar/AppBar";
 import FormSearchMovies from "components/FormSearchMovies/FormSearchMovies";
-import { getSearchQueryMovies } from "services/getSearchMovies";
 import MoviesList from "components/MoviesList/MoviesList";
-
 import Loader from "components/Loader/Loader";
-
+import { getSearchQueryMovies } from "services/getSearchMovies";
 
 const Movies = () => {
 
-    const [items, setItems] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-      const [searchParams, setSearchParams] = useSearchParams();
-      const query = searchParams.get("query");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query");
 
+  const onChangeQuery = query => {
+    setSearchParams({ query : query });
+    setItems([]);
+  };
+
+  useEffect(() => {
   
-    const onChangeQuery = query => {
-     setSearchParams({query : query});
-       setItems([]);
-    };
+      const getSearchMovies = async () => {
 
-    useEffect(() => {
-  
-        const getSearchMovies = async () => {
-
-          if(!query) {
-            return;
-          }      
-          setIsLoading(true);
-    
-          try {
-            const data = await getSearchQueryMovies({query});
-            setItems(data)
-            } 
-            catch (error) {
-              setError(error);
-            } 
-            finally {
-              setIsLoading(false);
-          }
+        if(!query) {
+          return;
+        }  
+        setIsLoading(true);
+      
+        try {
+          const data = await getSearchQueryMovies({query});
+          setItems(data)
+        } 
+        catch (error) {
+          setError(error);
+        } 
+        finally {
+          setIsLoading(false);
         }
+      }
     
-        getSearchMovies();
+      getSearchMovies();
     
-      }, [query])
+  }, [query])
     
-
-    
-
-
-    return (
-        <>
-        <AppBar/>
-        <FormSearchMovies onSubmit={onChangeQuery}/>
-        {items.length !== 0 && <MoviesList items={items} />}
-
-        
-        {isLoading && <Loader/>}
-        {error && <p>Please try again later!</p>}
-    
-        </>
-    )
+  return (
+    <>
+      <AppBar/>
+      <FormSearchMovies onSubmit={onChangeQuery}/>
+      {items.length !== 0 && <MoviesList items={items} />}
+      {isLoading && <Loader/>}
+      {error && <p>Please try again later!</p>}
+    </>
+  )
 }
 
 export default Movies;
