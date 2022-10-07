fd06635657f54c7572959891cc6722ff7e63c18f
Darya <daryabobur@gmail.com>
2022-10-07 13:12:04 +0300

    Create ReviewsMovieInfoStyled.js

---
+++ src/components/ReviewsMovie/ReviewsMovieInfo/ReviewsMovieInfoStyled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ReviewsList = styled.ul`
+    padding-top: 20px;
+    padding-bottom: 30px;
+
+    li {
+        margin-bottom: 15px;
+    }
+
+    p {
+        :not(:last-child) {
+            margin-bottom: 15px;
+        } 
+    }
+`
+
