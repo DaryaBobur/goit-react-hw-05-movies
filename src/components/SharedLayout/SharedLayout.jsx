7b69c377f28c71740eeb4393085928c0814f1eb0
Darya <daryabobur@gmail.com>
2022-10-07 13:12:10 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,18 +1,16 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import AppBar from "components/AppBar/AppBar";
-
 import Loader from "components/Loader/Loader";
 
-
-const SharedLayout =() => {
+const SharedLayout = () => {
     return(
-        <div>
-<AppBar/>
-<Suspense fallback={<Loader />}>
-<Outlet />
-</Suspense>
-        </div>
+        <>
+            <AppBar/>
+            <Suspense fallback={<Loader />}>
+                <Outlet />
+            </Suspense>
+        </>
     )
 }
 
