2a3bad08906da513a9726fff1df9420aa0097641
Darya <daryabobur@gmail.com>
2022-10-07 13:11:58 +0300

    Update MoviesList.jsx

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,19 +1,26 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { MovieItem, MovieLink } from "./MovieListStyled";
 
 const MoviesList = ({ items }) => {
     const item = items.data.results;
-
     const location = useLocation();
 
-return (
-    <ul>
-        {item.map(({title, id}) => (
-        <li key={id}>
-        <NavLink state={{from: location}} to={`${id}`}><h2>{title}</h2></NavLink>
-        </li>
-        ))} 
-    </ul>
-)
+    return (
+        <ul>
+            {item.map(({title, id}) => (
+                <MovieItem key={id}>
+                    <MovieLink state={{from: location}} to={`/movies/${id}`}>
+                        <h2>{title}</h2>
+                    </MovieLink>
+                </MovieItem>
+            ))} 
+        </ul>
+    )
+}
+
+MoviesList.propTypes = {
+    items: PropTypes.object.isRequired,
 }
 
 export default MoviesList;
