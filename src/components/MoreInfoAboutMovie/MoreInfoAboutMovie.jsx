f2c72a7778c8bbc195c7d7d460bd24dddb78321d
Darya <daryabobur@gmail.com>
2022-10-07 13:11:49 +0300

    Update MoreInfoAboutMovie.jsx

--- src/components/MoreInfoAboutMovie/MoreInfoAboutMovie.jsx
+++ src/components/MoreInfoAboutMovie/MoreInfoAboutMovie.jsx
@@ -1,17 +1,25 @@
 import { NavLink, useLocation } from 'react-router-dom';
-
+import { Wrap } from './MoreInfoAboutMovieStyled';
 const MoreInfoAboutMovie = () => {
+    
     const location = useLocation();
     const from = location.state?.from ?? "/movies";
-return (
-    <>
-        <h2>Additional information</h2>
-        <ul>
-        <li><NavLink state={{from}} to="cast">Cast</NavLink></li>
-        <li><NavLink state={{from}} to="reviews">Reviews</NavLink></li>
-        </ul>
-    </>
-)
+
+    return (
+        <Wrap>
+            <h2>Additional information</h2>
+
+            <ul>
+                <li>
+                    <NavLink state={{from}} to="cast">Cast</NavLink>
+                </li>
+                
+                <li>
+                    <NavLink state={{from}} to="reviews">Reviews</NavLink>
+                </li>
+            </ul>
+        </Wrap>
+    )
 }
 
 export default MoreInfoAboutMovie;
