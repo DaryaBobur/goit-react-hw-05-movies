effc65e75d185c3145bcd5692736c63d4c6d20bb
Darya <daryabobur@gmail.com>
2022-10-07 13:12:15 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,53 +1,49 @@
 import { useState, useEffect } from "react";
-
 import AppBar from "components/AppBar/AppBar";
 import Loader from "components/Loader/Loader";
-import { getSearchTrendingMovies } from "services/getSearchMovies";
 import MoviesList from "components/MoviesList/MoviesList";
-
+import { getSearchTrendingMovies } from "services/getSearchMovies";
+import { Title } from "./HomeStyled";
+import { Container } from "components/MoviesList/MovieListStyled";
 
 const Home = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-
-
     useEffect(() => {
 
         const fetchTrendingMovies = async () => {
-            setIsLoading(true);
-
-          
-            try {
-                
-               const data = await getSearchTrendingMovies();
-               setItems(data);
-            
-        } catch (error) {
+            setIsLoading(true);      
+            try {  
+                const data = await getSearchTrendingMovies();
+                setItems(data);
+            } 
+            catch (error) {
                 setError(error)
-            } finally {
+            } 
+            finally {
                 setIsLoading(false);
             }
         }
 
-            fetchTrendingMovies();
-    }, [])
+        fetchTrendingMovies();
 
+    }, [])
 
     return (
         <>
             <AppBar/>
+
             {items.length !== 0 && (
-            <>
-            <h1>Trending movies</h1>
-            <MoviesList items={items}/>
-            </>
+                <Container>
+                    <Title>Trending movies</Title>
+                    <MoviesList items={items}/>
+                </Container>
             )}
 
             {isLoading && <Loader/>}
-            {error && <p>Please try again later!</p>}
-          
+            {error && <p>Please try again later!</p>}         
         </>
     )
 }
