80a99867c9e9461ae1a60109bad76b31d7479bad
Darya <daryabobur@gmail.com>
2022-10-07 13:11:51 +0300

    Create MoreInfoAboutMovieStyled.js

---
+++ src/components/MoreInfoAboutMovie/MoreInfoAboutMovieStyled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.div`
+    padding-top: 30px;
+    padding-bottom: 10px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+
+    h2 {
+        margin-bottom: 15px;
+        font-size: 18px;
+    }
+`
